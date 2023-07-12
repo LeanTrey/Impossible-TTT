457168593c1f09c7d4a3a7cc15ecd7ae05716190
Cullin Moran <cMoran@vtestify.com>
2023-07-12 12:41:24 -0400

    cjm code review changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+/*
+  The app compiles and runs properly, but when building, you can see some helpful errors in the terminal from eslint which should probably be addressed!
+  In our vTestify repos, we have automated systems which prevent merging any PRs with outstanding eslint warnings. Terminal output:
+
+    Compiled with warnings.
+
+    [eslint]
+    src/App.js
+      Line 30:12:  'availableMoves' is defined but never used                                                                                                                                                                                                     no-unused-vars
+      Line 119:6:  React Hook useEffect has missing dependencies: 'minimax', 'result', and 'score'. Either include them or remove the dependency array. You can also do a functional update 'setScore(s => ...)' if you only need 'score' in the 'setScore' call  react-hooks/exhaustive-deps
+
+    Search for the keywords to learn more about each warning.
+    To ignore, add // eslint-disable-next-line to the line before.
+*/
+
 // We're importing the required modules for our app, such as styles and components.
-import React from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+
+// Better to consolidate imports from the same package, moved these into the import above
+// import { useState, useEffect } from 'react';
+
 import './App.css';
 import Board from "./Board";
 import Square from "./Square";
-import { useState, useEffect } from 'react';
 import Modal from 'react-modal';
 
 // Here we're defining the default state for our squares. It's an array of nine null elements.
@@ -21,21 +39,21 @@ Modal.setAppElement('#root');
 // We're using the useState hook to manage the state for squares and result in our component.
 function App() {
   const [squares, setSquares] = useState(defaultSquares());
-  const [result, setResult] = useState(null);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [score, setScore] = useState({ wins: 0, ties: 0, losses: 0 });
 
-  // This function identifies the available moves in the game. 
-  // It looks at each square, if it's null, it means the square is not occupied.
-  function availableMoves(squares) {
-    return squares
-      .map((square, index) => square === null ? index : null)
-      .filter(val => val !== null);
-  }
+  // One of the eslint warnings was that this function isn't used.
+  // I've commented it out but it could be removed.
+  // function availableMoves(squares) {
+  //   return squares
+  //     .map((square, index) => square === null ? index : null)
+  //     .filter(val => val !== null);
+  // }
 
   // This function calculates the result of the game. 
   // It checks if any of the winning combinations is filled by a single player.
-  function calculateResult(squares) {
+  // * Root-level functions not wrapped in `useCallback` will be redeclared every render, causing reduced performance.
+  const calculateResult = useCallback((squares) => {
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
@@ -44,10 +62,11 @@ function App() {
     }
      // If no more squares are available and no winner, then it's a tie
     return squares.filter(val => val === null).length ? null : 'tie';
-  }
+  }, []);
+  
   // function used to calculate the best move for the computer player.
   // It returns an object with the best score and the move that leads to that score.
-  function minimax(squares, depth, isMaximizingPlayer) {
+  const minimax = useCallback((squares, depth, isMaximizingPlayer) => {
     const result = calculateResult(squares);
     
     // Returns the score of the game after it ends
@@ -88,44 +107,58 @@ function App() {
       }
       return { score: bestScore, move: move };
     }
-  }
+  }, [calculateResult]);
 
-  // This useEffect hook runs whenever the state of squares or result changes.
+  /**
+   * The section below (large useEffect) could be made more efficient & less complicated
+   * by splitting up responsibility for calculating different states:
+   **/
+
+  // Anytime `squares` is updated, this code will run once and store `true` or `false` in `isPlayerTurn`.
+  // If we want to know whether it's the computer's turn we can just use `!isPlayerTurn`
+  const isPlayerTurn = useMemo(
+    () =>
+      // Cool filter trick I learned from @LukasBarry, selects only truthy values (rejecting null or undefined)
+      squares.filter(Boolean).length % 2 === 0,
+    [squares]
+  );
+
+  // Anytime a value in the dependency array `[squares]` changes,
+  // re-run `calculateResult` one time and store the result in this memoized `result` variable.
+  const result = useMemo(() => calculateResult(squares), [calculateResult, squares]);
+
+  // Handle taking the computer's turn
   useEffect(() => {
-    const isComputerTurn = squares.filter(square => square !== null).length % 2 === 1;
-    const currentResult = calculateResult(squares);
+    if (isPlayerTurn) return; // Do nothing if it's not the computer's turn
+    if (result) return; // Do nothing if the game is over
+
+    const bestMove = minimax(squares.slice(), 0, true).move;
+    const newSquares = squares.slice();
+    newSquares[bestMove] = 'o';
+    setSquares(newSquares);
+  }, [isPlayerTurn, result, squares, minimax]);
+
+  // Handle the game ending (show modal & update score)
+  useEffect(() => {
+    if (!result) return;
     
-    if (currentResult && !result) {
-      setResult(currentResult);
-      setModalIsOpen(true);
-      if (currentResult === 'x') setScore({ ...score, wins: score.wins + 1 });
-      else if (currentResult === 'o') setScore({ ...score, losses: score.losses + 1 });
-      else if (currentResult === 'tie') setScore({ ...score, ties: score.ties + 1 });
-      return;
-    }
+    setModalIsOpen(true);
 
-    // This function will place the computer's move at the given index.
-    const putComputerAt = index => {
-      let newSquares = squares.slice();
-      newSquares[index] = 'o';
-      setSquares(newSquares);
-    };
-
-    // If it's the computer's turn and there's no result yet, it calculates the best move and makes it.
-    if (isComputerTurn && !currentResult) {
-      const bestMove = minimax(squares.slice(), 0, true).move;
-      putComputerAt(bestMove);
-    }
-  }, [squares]);
+    // Updating state based on previous state can cause errors unless you set the state using a function param.
+    // Refactored based on details here: https://react.dev/reference/react/useState#updating-state-based-on-the-previous-state
+    if (result === 'x') setScore(score => ({ ...score, wins: score.wins + 1 }));
+    else if (result === 'o') setScore(score => ({ ...score, losses: score.losses + 1 }));
+    else if (result === 'tie') setScore(score => ({ ...score, ties: score.ties + 1 }));
+  }, [result, isPlayerTurn]);
 
   // This function handles the player's move when a square is clicked.
-  function handleSquareClick(index) {
-    const isPlayerTurn = squares.filter(square => square !== null).length % 2 === 0;
-    const squareIsFree = squares[index] === null;
+  // * The functions below should all be wrapped in `useCallback` as well, with dependencies set accordingly:
+  const handleSquareClick = useCallback((index) => {
+    if (!isPlayerTurn || result) return;
 
-    if (!squareIsFree || !isPlayerTurn || result) {
-      return;
-    }
+    // Avoid calculating this until absolutely needed by checking it after the two things above
+    const squareIsFree = squares[index] === null;
+    if (!squareIsFree) return;
 
     // Play into square if it is the player's turn and there is no game result
     if (isPlayerTurn && !result) {
@@ -133,17 +166,32 @@ function App() {
       newSquares[index] = 'x';
       setSquares(newSquares);
     }
-  }
+  }, [result, squares, isPlayerTurn]);
   // This function resets the score
-  function resetScore() {
+  const resetScore = useCallback(() => {
     setScore({ wins: 0, ties: 0, losses: 0 });
-  }
+  }, []);
   // This function resets the game
-  function resetGame() {
+  const resetGame = useCallback(() => {
     setSquares(defaultSquares());
-    setResult(null);
     setModalIsOpen(false);
-  }
+  }, []);
+
+  // Agree with @LukasBarry about the nested ternaries. Here's one way to make that code easier to read,
+  // even if it's a little verbose/repetitive. In a real project I'd probably extract the modal into its own component/JS file.
+  const resultModalUi = useMemo(() => {
+    switch(result) {
+      case 'tie': return (
+        <div className="result yellow">Tie!</div>
+      );
+      case 'o': return (
+        <div className="result red">You Lose!</div>
+      );
+      default: return (
+        <div className="result green">You Win!</div>
+      );
+    }
+  }, [result]);
 
   // Here we're rendering our Board, Squares, and some buttons.
   return (
@@ -175,9 +223,7 @@ function App() {
         overlayClassName="ReactModal__Overlay"
       >
         <h2>Game Over</h2>
-        <div className={`result ${result === 'tie' ? 'yellow' : (result === 'o' ? 'red' : 'green')}`}>
-          {result === 'tie' ? 'Tie!' : (result === 'o' ? 'You Lose!' : 'You Win!')}
-        </div>
+        {resultModalUi}
         <button className='play-again-btn' onClick={resetGame}>Play Again</button>
       </Modal>
     </main>
@@ -185,5 +231,3 @@ function App() {
 }
 
 export default App;
-
-
